34ca7b9533f0c5e4cf16e8fbf50fb2606b3fb986
agrawalgaurav <gaurav.agrawal@huawei.com>
2021-03-08 21:21:35 +0530

    Model updation for package synchronization in AppRuleMgr.

--- models/appd_rule_config.go
+++ models/appd_rule_config.go
@@ -16,45 +16,91 @@
 
 package models
 
+import "github.com/astaxie/beego/orm"
+
 type SampleStruct struct {
 	SomeData string
 }
 
+// Init application info record
+func init() {
+	orm.RegisterModel(new(AppdRule))
+	orm.RegisterModel(new(AppTrafficRule))
+	orm.RegisterModel(new(AppDnsRule))
+	orm.RegisterModel(new(TrafficFilter))
+	orm.RegisterModel(new(DstInterface))
+	orm.RegisterModel(new(TunnelInfo))
+}
+
 type AppdRule struct {
-	AppTrafficRule []AppTrafficRule `json:"appTrafficRule" validate:"min=0,dive,max=16" `
-	AppDnsRule     []AppDnsRule     `json:"appDnsRule" validate:"min=0,dive,max=32" `
-	AppName        string           `json:"appName" validate:"required,max=128,validateName"`
-	AppSupportMp1  bool             `json:"appSupportMp1,omitempty"`
+	AppdRuleId     string            `orm:"pk" json:"appdRuleId,omitempty"`
+	TenantId       string            `json:"tenantId,omitempty"`
+	AppInstanceId  string            `json:"appInstanceId,omitempty"`
+	AppName        string            `json:"appName" validate:"required,max=128,validateName"`
+	AppSupportMp1  bool              `json:"appSupportMp1,omitempty"`
+	AppTrafficRule []*AppTrafficRule `orm:"reverse(many);on_delete(set_null)" json:"appTrafficRule" validate:"min=0,dive,max=16" `
+	AppDnsRule     []*AppDnsRule     `orm:"reverse(many);on_delete(set_null)" json:"appDnsRule" validate:"min=0,dive,max=32" `
 }
 
 // Represents traffic rule model
 type AppTrafficRule struct {
-	TrafficRuleId    string          `json:"trafficRuleId" validate:"required,max=128"`
-	FilterType       string          `json:"filterType" validate:"required,oneof=FLOW PACKET"`
-	Priority         int             `json:"priority" validate:"required,gt=0,max=255"`
-	Action           string          `json:"action" validate:"required,oneof=DROP PASSTHROUGH"`
-	AppTrafficFilter []TrafficFilter `json:"trafficFilter" validate:"required,dive"`
+	TrafficRuleId    string           `orm:"pk" json:"trafficRuleId" validate:"required,max=128"`
+	FilterType       string           `json:"filterType" validate:"required,oneof=FLOW PACKET"`
+	Priority         int              `json:"priority" validate:"required,gt=0,max=255"`
+	Action           string           `json:"action" validate:"required,oneof=DROP PASSTHROUGH"`
+	AppTrafficFilter []*TrafficFilter `orm:"reverse(many);on_delete(set_null)" json:"trafficFilter" validate:"required,dive"`
+	DstInterface     []*DstInterface  `orm:"reverse(many);on_delete(set_null)" json:"dstInterface" validate:"omitempty,dive"`
+	AppdRule         *AppdRule        `orm:"rel(fk)"`
 }
 
-// Represents dns rule model
-type AppDnsRule struct {
-	DnsRuleId     string `json:"dnsRuleId" validate:"required,max=128"`
-	DomainName    string `json:"domainName" validate:"required,max=128"`
-	IpAddressType string `json:"ipAddressType" validate:"required,oneof=IP_V4 IP_V6"`
-	IpAddress     string `json:"ipAddress" validate:"required,ip_addr"`
-	TTL           int    `json:"ttl" validate:"omitempty,gte=0,max=4294967295"`
+// Destination interface
+type DstInterface struct {
+	DstInterfaceId string          `orm:"pk" json:"dstInterfaceId" validate:"omitempty,max=128"`
+	InterfaceType  string          `json:"interfaceType" validate:"omitempty`
+	SrcMacAddress  string          `json:"srcMacAddress" validate:"omitempty`
+	DstMacAddress  string          `json:"dstMacAddress" validate:"omitempty`
+	DstIpAddress   string          `json:"dstIpAddress" validate:"omitempty`
+	TunnelInfo     *TunnelInfo     `orm:"reverse(one);on_delete(set_null)"`
+	AppTrafficRule *AppTrafficRule `orm:"rel(fk)"`
+}
+
+// Tunnel information
+type TunnelInfo struct {
+	TunnelInfoId       string        `orm:"pk" json:"tunnelInfoId" validate:"omitempty,max=128"`
+	TunnelType         string        `json:"tunnelType" validate:"omitempty`
+	TunnelDstAddress   string        `json:"tunnelDstAddress" validate:"omitempty`
+	TunnelSrcAddress   string        `json:"tunnelSrcAddress" validate:"omitempty`
+	TunnelSpecificData string        `json:"tunnelSpecificData" validate:"omitempty`
+	DstInterface       *DstInterface `orm:"rel(one)"`
 }
 
 // Represents traffic filter model
 type TrafficFilter struct {
-	SrcAddress []string `json:"srcAddress" validate:"omitempty,dive,cidr"`
-	SrcPort    []string `json:"srcPort" validate:"omitempty,dive,gt=0,lte=65535"`
-	DstAddress []string `json:"dstAddress" validate:"omitempty,dive,cidr"`
-	DstPort    []string `json:"dstPort" validate:"omitempty,dive,gt=0,lte=65535"`
-	Protocol   []string `json:"protocol" validate:"omitempty,dive,validateProtocol"`
-	Qci        int      `json:"qCI" validate:"omitempty"`
-	Dscp       int      `json:"dSCP" validate:"omitempty"`
-	Tc         int      `json:"tC" validate:"omitempty"`
+	TrafficFilterId  string          `orm:"pk"`
+	SrcAddress       []string        `orm:"-" json:"srcAddress" validate:"omitempty,dive,cidr"`
+	SrcPort          []string        `orm:"-" json:"srcPort" validate:"omitempty,dive,gt=0,lte=65535"`
+	DstAddress       []string        `orm:"-" json:"dstAddress" validate:"omitempty,dive,cidr"`
+	DstPort          []string        `orm:"-" json:"dstPort" validate:"omitempty,dive,gt=0,lte=65535"`
+	Protocol         []string        `orm:"-" json:"protocol" validate:"omitempty,dive,validateProtocol"`
+	Qci              int             `json:"qCI" validate:"omitempty"`
+	Dscp             int             `json:"dSCP" validate:"omitempty"`
+	Tc               int             `json:"tC" validate:"omitempty"`
+	Tag              []string        `orm:"-" json:"tag" validate:"omitempty"`
+	SrcTunnelAddress []string        `orm:"-" json:"srcTunnelAddress" validate:"omitempty"`
+	DstTunnelAddress []string        `orm:"-" json:"dstTunnelAddress" validate:"omitempty"`
+	SrcTunnelPort    []string        `orm:"-" json:"drcTunnelPort" validate:"omitempty"`
+	DstTunnelPort    []string        `orm:"-" json:"dstTunnelPort" validate:"omitempty"`
+	AppTrafficRule   *AppTrafficRule `orm:"rel(fk)"`
+}
+
+// Represents dns rule model
+type AppDnsRule struct {
+	DnsRuleId     string    `orm:"pk" json:"dnsRuleId" validate:"required,max=128"`
+	DomainName    string    `json:"domainName" validate:"required,max=128"`
+	IpAddressType string    `json:"ipAddressType" validate:"required,oneof=IP_V4 IP_V6"`
+	IpAddress     string    `json:"ipAddress" validate:"required,ip_addr"`
+	TTL           int       `json:"ttl" validate:"omitempty,gte=0,max=4294967295"`
+	AppdRule      *AppdRule `orm:"rel(fk)"`
 }
 
 // Represents operation progress model
